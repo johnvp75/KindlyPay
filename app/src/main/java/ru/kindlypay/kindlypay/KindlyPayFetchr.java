91dcf1c372e81cbb0b01d45cb1ca3bb2447ebd25
johnvp75 <johnvp@yandex.ru>
2017-09-22 15:38:55 +0300

    Login fetch

---
+++ app/src/main/java/ru/kindlypay/kindlypay/KindlyPayFetchr.java
@@ -0,0 +1,49 @@
+package ru.kindlypay.kindlypay;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.security.AccessController.getContext;
+
+/**
+ * Created by User on 22.09.2017.
+ */
+
+public class KindlyPayFetchr {
+
+    public String getUserId(User mUser,String mUrl) throws IOException {
+        URL url = new URL(mUrl);
+        Map<String,Object> params = new LinkedHashMap<>();
+        params.put("login", mUser.getPhoneNumber());
+        params.put("password", mUser.getPassword());
+
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        for (int c; (c = in.read()) >= 0;)
+            sb.append((char)c);
+        return sb.toString();
+    }
+}
