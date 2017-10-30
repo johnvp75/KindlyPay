fa35abc09de169618699156355a06a9deccbf456
johnvp75 <johnvp@yandex.ru>
2017-10-30 16:20:01 +0300

    generak refactoring

--- app/src/main/java/ru/kindlypay/kindlypay/NavigationDrawerFragment.java
+++ app/src/main/java/ru/kindlypay/kindlypay/NavigationDrawerFragment.java
@@ -90,7 +90,7 @@ public class NavigationDrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mDrawerListView = (ListView) inflater.inflate(
-                R.layout.drawer_user_page, container, false);
+                R.layout.drawer_main, container, false);
         mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
